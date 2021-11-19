a60f8197f74c578eb0087510453f25ae5b9aacc0
keppo4an <91660441+keppo4an@users.noreply.github.com>
2021-11-19 18:57:28 +0300

    Create 1.cs

---
+++ 1.cs
@@ -0,0 +1,37 @@
+ using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+    Console.WriteLine("Введите число, которое соответсвует дню недели");
+    int day = int.Parse(Console.ReadLine());
+    switch(day){
+      case 1:
+      Console.WriteLine("Понедельник");
+      break;
+      case 2:
+      Console.WriteLine("Вторник");
+      break;
+      case 3:
+      Console.WriteLine("Среда");
+      break;
+      case 4:
+      Console.WriteLine("Четверг");
+      break;
+      case 5:
+      Console.WriteLine("Пятница");
+      break;
+      case 6:
+      Console.WriteLine("Суббота");
+      break;
+      case 7:
+      Console.WriteLine("Воскресенье");
+      break;
+      default:
+      Console.WriteLine("данного дня недели несущесвует");
+      break;
+      
+    }
+  }
+}
